f181f9b2f0a2c2bdb1544e109721ab460318b9fa
Rene Delgado <rene854@gmail.com>
2021-03-11 17:26:12 -0300

    fix message

--- services/http.go
+++ services/http.go
@@ -15,7 +15,7 @@ type HTTPService struct{}
 func (s *HTTPService) Get() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		acessToken, _ := c.Get("access_token")
-		log.Printf("Hello from adapter. acess_token: %v\n", fmt.Sprint(acessToken))
+		log.Printf("Hello from http service. acess_token: %v\n", fmt.Sprint(acessToken))
 		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "test": "123"})
 	}
 }
